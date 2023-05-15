73860b2bf753fc037f8f0f3da21b29aac4d55771
Kavin Dail <113409087+kavindail@users.noreply.github.com>
2023-05-15 12:35:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const PAGE_SIZE = 10;
 let currentPage = 1;
 let pokemons = [];
 
+const paginationElement = document.getElementById('pagination');
+const pokemonElement = document.getElementById('pokemon');
+const displayInfoElement = document.getElementById('displayInfo');
+
 const updatePaginationDiv = (currentPage, numPages) => {
-  $('#pagination').empty();
+  paginationElement.innerHTML = '';
 
   if (currentPage > 1) {
-    $('#pagination').append(`
+    paginationElement.insertAdjacentHTML('beforeend', `
       <li class="page-item">
         <button class="page-link previousButton" value="${currentPage - 1}">Previous</button>
       </li>
@@ -17,7 +21,7 @@ const updatePaginationDiv = (currentPage, numPages) => {
   const endPage = Math.min(numPages, startPage + 4);
 
   for (let i = startPage; i <= endPage; i++) {
-    $('#pagination').append(`
+    paginationElement.insertAdjacentHTML('beforeend', `
       <li class="page-item${i === currentPage ? ' active' : ''}">
         <button class="page-link numberedButtons" value="${i}">${i}</button>
       </li>
@@ -25,7 +29,7 @@ const updatePaginationDiv = (currentPage, numPages) => {
   }
 
   if (currentPage < numPages) {
-    $('#pagination').append(`
+    paginationElement.insertAdjacentHTML('beforeend', `
       <li class="page-item">
         <button class="page-link nextButton" value="${currentPage + 1}">Next</button>
       </li>
@@ -34,41 +38,50 @@ const updatePaginationDiv = (currentPage, numPages) => {
 };
 
 const updateDisplayInfo = (total, displayed) => {
-  $('#displayInfo').text(`Total Pokémon: ${total} | Displayed Pokémon: ${displayed}`);
+  displayInfoElement.textContent = `Total Pokémon: ${total} | Displayed Pokémon: ${displayed}`;
 };
 
-const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
-  selected_pokemons = pokemons.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE);
+const paginate = async (currentPage, pageSize, data) => {
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const selectedData = data.slice(startIndex, endIndex);
 
-  $('#pokemon').empty();
-  selected_pokemons.forEach(async (pokemon) => {
-    const res = await axios.get(pokemon.url);
-    $('#pokemon').append(`
-      <div class="pokeCard card" pokeName=${res.data.name}>
-        <h3>${res.data.name.toUpperCase()}</h3> 
-        <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pokeModal">
-          More
-        </button>
-      </div>
-    `);
-  });
+  pokemonElement.innerHTML = '';
 
-  updateDisplayInfo(pokemons.length, selected_pokemons.length);
+  for (const item of selectedData) {
+    const res = await axios.get(item.url);
+
+    const divElement = document.createElement('div');
+    divElement.classList.add('pokeCard', 'card');
+    divElement.setAttribute('pokeName', res.data.name);
+    divElement.innerHTML = `
+      <h3>${res.data.name.toUpperCase()}</h3>
+      <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pokeModal">
+        More
+      </button>
+    `;
+
+    pokemonElement.appendChild(divElement);
+  }
+
+  updateDisplayInfo(data.length, selectedData.length);
 };
 
 const fetchPokemonTypes = async () => {
   const response = await axios.get('https://pokeapi.co/api/v2/type');
   const types = response.data.results;
 
-  const typesCheckboxes = types.map((type) => `
-    <div class="form-check form-check-inline">
-      <input class="form-check-input typeCheckbox" type="checkbox" value="${type.name}" id="${type.name}">
-      <label class="form-check-label" for="${type.name}">${type.name}</label>
-    </div>
-  `).join('');
+  const typesCheckboxes = types
+    .map(type => `
+      <div class="form-check form-check-inline">
+        <input class="form-check-input typeCheckbox" type="checkbox" value="${type.name}" id="${type.name}">
+        <label class="form-check-label" for="${type.name}">${type.name}</label>
+      </div>
+    `)
+    .join('');
 
-  $('#typeFilter').html(typesCheckboxes);
+  document.getElementById('typeFilter').innerHTML = typesCheckboxes;
 };
 
 const pokemonTypeCache = new Map();
@@ -80,8 +93,8 @@ const fetchPokemonDetails = async (pokemon) => {
 
   try {
     const res = await axios.get(pokemon.url);
-    const types = res.data.types.map((type) => type.type.name);
-    pokemonTypeCache.set(pokemon.name, types);
+    const types = res.data.types.map(type => type.type.name);
+    pokemon TypeCache.set(pokemon.name, types);
     return types;
   } catch (error) {
     console.error('Error', error);
@@ -90,9 +103,11 @@ const fetchPokemonDetails = async (pokemon) => {
 };
 
 const filterPokemons = async (selectedTypes) => {
-  const filteredPokemons = []; for (const pokemon of pokemons) {
+  const filteredPokemons = [];
+  
+  for (const pokemon of pokemons) {
     const types = await fetchPokemonDetails(pokemon);
-    if (selectedTypes.length === 0 || selectedTypes.every((type) => types.includes(type))) {
+    if (selectedTypes.length === 0 || selectedTypes.every(type => types.includes(type))) {
       filteredPokemons.push(pokemon);
     }
   }
@@ -103,63 +118,60 @@ const filterPokemons = async (selectedTypes) => {
 };
 
 const setup = async () => {
-  $('#pokemon').empty();
-  let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+  pokemonElement.innerHTML = '';
+  const response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
   pokemons = response.data.results;
 
   await fetchPokemonTypes();
 
-  $('body').on('click', '.pokeCard', async function (e) {
-    const pokemonName = $(this).attr('pokeName')
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    const types = res.data.types.map((type) => type.type.name)
-    $('.modal-body').html(`
+  document.body.addEventListener('click', async (e) => {
+    if (e.target.classList.contains('pokeCard')) {
+      const pokemonName = e.target.getAttribute('pokeName');
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      const types = res.data.types.map(type => type.type.name);
+
+      const modalBodyElement = document.querySelector('.modal-body');
+      modalBodyElement.innerHTML = `
         <div style="width:200px">
-        <img src="${res.data.sprites.other['official-artwork'].front_default}" alt="${res.data.name}"/>
-        <div>
-        <h3>Abilities</h3>
+          <img src="${res.data.sprites.other['official-artwork'].front_default}" alt="${res.data.name}"/>
+          <div>
+            <h3>Abilities</h3>
+            <ul>
+              ${res.data.abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
+            </ul>
+          </div>
+          <div>
+            <h3>Stats</h3>
+            <ul>
+              ${res.data.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
+            </ul>
+          </div>
+        </div>
+        <h3>Types</h3>
         <ul>
-        ${res.data.abilities.map((ability) => `<li>${ability.ability.name}</li>`).join('')}
+          ${types.map(type => `<li>${type}</li>`).join('')}
         </ul>
-        </div>
-        <div>
-        <h3>Stats</h3>
-        <ul>
-        ${res.data.stats.map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
-        </ul>
+      `;
 
-        </div>
-
-        </div>
-          <h3>Types</h3>
-          <ul>
-          ${types.map((type) => `<li>${type}</li>`).join('')}
-          </ul>
-      
-        `)
-    $('.modal-title').html(`
+      const modalTitleElement = document.querySelector('.modal-title');
+      modalTitleElement.innerHTML = `
         <h2>${res.data.name.toUpperCase()}</h2>
         <h5>${res.data.id}</h5>
-        `)
+      `;
+    }
   });
 
-  $('body').on('change', '.typeCheckbox', async function () {
-    const selectedTypes = [];
-    $('.typeCheckbox:checked').each(function () {
-      selectedTypes.push($(this).val());
-    });
-
+  document.body.addEventListener('change', async () => {
+    const selectedTypes = Array.from(document.querySelectorAll('.typeCheckbox:checked')).map(checkbox => checkbox.value);
     await filterPokemons(selectedTypes);
   });
 
-  $('body').on('click', ".numberedButtons, .previousButton, .nextButton", async function (e) {
-    currentPage = Number(e.target.value);
-    const selectedTypes = [];
-    $('.typeCheckbox:checked').each(function () {
-      selectedTypes.push($(this).val());
-    });
-
-    await filterPokemons(selectedTypes);
+  document.body.addEventListener('click', async (e) => {
+    if (e.target.classList.contains('numberedButtons') || e.target.classList.contains('previousButton') || e.target.classList.contains('nextButton')) {
+      currentPage = Number(e.target.value);
+      const selectedTypes = Array.from(document.querySelectorAll('.typeCheckbox:checked')).map(checkbox => checkbox.value);
+      await filterPokemons(selectedTypes);
+    }
   });
 
   const numPages = Math.ceil(pokemons.length / PAGE_SIZE);
@@ -167,4 +179,4 @@ const setup = async () => {
   updatePaginationDiv(currentPage, numPages);
 };
 
-$(document).ready(setup);
+document.addEventListener('DOMContentLoaded', setup);
